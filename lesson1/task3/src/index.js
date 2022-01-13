56609094ec3c7ea45be6f32b8b32a1fff1315907
Hostlife21 <sima.serafim2@mail.ru>
2022-01-14 00:24:12 +0300

    fix: script

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -6,8 +6,8 @@ const rootElement = document.querySelector('#root');
 
 const element = (
   <div className="greeting">
-    <h1 className="greeting__title">Hello, world</h1>
-    <p className="greeting__text">I'm learning React</p>
+    <h1 className="greeting__title">Hello, world!</h1>
+    <p className="greeting__text">I am learning React</p>
   </div>
 );
 
