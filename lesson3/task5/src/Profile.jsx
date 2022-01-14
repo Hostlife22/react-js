017920885fa4ae4d88bd4d3694e73e3ea3c0d653
Hostlife21 <sima.serafim2@mail.ru>
2022-01-15 01:46:43 +0300

    init task5

---
+++ lesson3/task5/src/Profile.jsx
@@ -0,0 +1,21 @@
+import moment from 'moment';
+import React from 'react';
+import './profile.scss';
+
+const formatDate = (date) => moment(date).format('DD MMM YYYY');
+
+const Profile = (props) => {
+  const fullName = `${props.userData.firstName} ${props.userData.lastName}`;
+  const birthDate = `Was born ${formatDate(props.userData.birthDate)} in ${
+    props.userData.birthPlace
+  }`;
+
+  return (
+    <div className="profile">
+      <div className="profile__name">{fullName}</div>
+      <div className="profile__birth">{birthDate}</div>
+    </div>
+  );
+};
+
+export default Profile;
