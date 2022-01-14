4a4593e4e5bd2362e1692d717a5f496b14362ad8
Hostlife21 <sima.serafim2@mail.ru>
2022-01-15 01:13:00 +0300

    fix: script

--- lesson3/task4/src/birthDate.js
+++ lesson3/task4/src/birthDate.js
@@ -2,7 +2,7 @@ export const birthDate = (date) => {
   const currentDate = new Date();
   const differentDate = currentDate - date;
 
-  const ageResult = differentDate / (24 * 3600 * 365.25 * 1000);
-
-  return ageResult >= 0 ? Math.trunc(ageResult) : 0;
+  return differentDate > 0
+    ? Math.trunc(differentDate / (24 * 3600 * 365.25 * 1000))
+    : 0;
 };
