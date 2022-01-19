4c08eb62427d7125299dbba3cd79a21ec48b4336
Hostlife21 <sima.serafim2@mail.ru>
2022-01-19 15:21:35 +0300

    init task5

--- lesson9/task5/src/index.jsx
+++ lesson9/task5/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from './Game';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<div></div>, rootElement);
+ReactDOM.render(<Game />, rootElement);
