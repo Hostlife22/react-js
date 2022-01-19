b2ff9a2aed2f8f9421c5de9bf8db979f755ba878
Hostlife22 <sima.serafim2021@mail.ru>
2022-01-19 15:47:10 +0300

    fix

--- lesson9/task5/src/Board.jsx
+++ lesson9/task5/src/Board.jsx
@@ -14,7 +14,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row ">
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
