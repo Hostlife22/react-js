da7e264816d4de89ed9af327e1a3c8683ee6347f
Hostlife22 <sima.serafim2021@mail.ru>
2022-01-20 00:23:34 +0300

    fix

--- lesson10/task3/src/TemperatureInput.jsx
+++ lesson10/task3/src/TemperatureInput.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
-const TemperatureInput = ({ scale, temperature, onTemperatureChange }) => {
-  return (
-    <fieldset>
-      <legend>
-        Enter temperature in {scale === 'c' ? 'Celsius' : 'Fahrenheit'}:
-      </legend>
-      <input
-        value={temperature}
-        onChange={(event) => onTemperatureChange(event.target.value)}
-      />
-    </fieldset>
-  );
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit',
 };
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
 
 export default TemperatureInput;
