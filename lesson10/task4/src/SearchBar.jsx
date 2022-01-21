991e9b8844b99ed0f8a48d3330ad000df02e527e
Hostlife22 <sima.serafim2021@mail.ru>
2022-01-21 17:13:02 +0300

    init task4

---
+++ lesson10/task4/src/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+  const { filterText, inStockOnly, onFilterTextChange, onInStockChange } =
+    props;
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={(e) => {
+          onFilterTextChange(e.target.value);
+        }}
+      />
+      <p>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => {
+            onInStockChange(e.target.checked);
+          }}
+        />{' '}
+        Only show products in stock
+      </p>
+    </form>
+  );
+};
+
+export default SearchBar;
