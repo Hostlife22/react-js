2f77e696225d1ffa63474a8f892ec8f806e65632
Hostlife22 <sima.serafim2021@mail.ru>
2022-02-07 16:03:35 +0300

    fix

--- redax/lesson1/task2/src/user-context.js
+++ redax/lesson1/task2/src/user-context.js
@@ -1,8 +1,3 @@
 import React from 'react';
 
-export const users = {
-  name: 'Nikola Tesla',
-  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-};
-
-export const UserContect = React.createContext({});
+export const UserContect = React.createContext();
