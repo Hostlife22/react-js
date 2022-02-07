4436cc582ae67a54cd6391efa471748a3cecdf25
Hostlife22 <sima.serafim2021@mail.ru>
2022-02-08 00:42:28 +0300

    afee

--- redax/lesson2/task3/src/index.js
+++ redax/lesson2/task3/src/index.js
@@ -1,10 +1,8 @@
 import store from './store';
 import { addUser, deleteUser } from './users.actions';
 
-store.dispatch(addUser({ id: 76, name: 'Alex' }));
-store.dispatch(addUser({ id: 78, name: 'Sarah' }));
-store.dispatch(addUser({ id: 74, name: 'Sem' }));
-store.dispatch(deleteUser(74));
+store.dispatch(addUser({ id: 76, name: 'Sarah' }));
+store.dispatch(deleteUser(76));
 
 store.subscribe(() => {
   const state = store.getState();
