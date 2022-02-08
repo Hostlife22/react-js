1739a11bb8b7a51d35ce5a03d1aa355fb3aedf3f
Hostlife22 <sima.serafim2021@mail.ru>
2022-02-08 18:54:09 +0300

    feat: task2

---
+++ redax/lesson3/task2/src/counter.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const counerReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  counerReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
