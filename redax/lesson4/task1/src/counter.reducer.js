64122a79ff64978b8b7cc7f79635f97905eefe68
Hostlife22 <sima.serafim2021@mail.ru>
2022-02-09 18:13:06 +0300

    init : tasl4

---
+++ redax/lesson4/task1/src/counter.reducer.js
@@ -0,0 +1,19 @@
+import { DECREMENT, INCREMENT } from './counter.actions';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    case INCREMENT:
+      return 0;
+
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
